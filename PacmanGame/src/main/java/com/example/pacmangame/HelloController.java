b7f0ddac43168cb8afdfed8766299238d757c62b
Filip Novák <91119101+filipusnovakus@users.noreply.github.com>
2022-12-16 12:22:07 +0100

    Add files via upload

--- PacmanGame/src/main/java/com/example/pacmangame/HelloController.java
+++ PacmanGame/src/main/java/com/example/pacmangame/HelloController.java
@@ -13,6 +13,7 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 
@@ -34,23 +35,60 @@ public class HelloController implements Initializable {
     GraphicsContext gc;
     public Label label;
     public VBox mainLayout;
+    Walls wall1;
+    Walls wall2;
+    Walls wall3;
+    Walls wall4;
+    Walls wall5;
+    Walls wall6;
+    Walls wall7;
+    Walls wall8;
+    Walls wall9;
+    Walls wall10;
+    Walls wall11;
+    Walls wall12;
+    Walls wall13;
+    Walls wall14;
+    Walls wall15;
+    Walls wall16;
+    Walls wall17;
+    Walls wall18;
     Entity entity;
     AnimationTimer mainLoop;
     ArrayList<String> inputList;
     ArrayList<Entity> entitiesList;
-
+    ArrayList<Walls> wallsList;
+    boolean block;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         inputList = new ArrayList<>();
         entitiesList = new ArrayList<>();
 
         gc = canvas.getGraphicsContext2D();
-        gc.setFill(Paint.valueOf("RED"));
-        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+ block = false;
+if (canvas.equals(wall1)) {
+    block = true;
+}
 
-
-
-        entity = new Entity(10, 10, 20, 20, Paint.valueOf("YELLOW"), 5.1f);
+        wall1 = new Walls(40,80,80,90,Paint.valueOf("BLUE"));
+        wall2 = new Walls(160,80,80,90,Paint.valueOf("BLUE"));
+        wall3 = new Walls(340,80,80,90,Paint.valueOf("BLUE"));
+        wall9 = new Walls(460, 80 , 80, 90,Paint.valueOf("BLUE"));
+        wall4 = new Walls(40, 250 , 80, 90,Paint.valueOf("BLUE"));
+        wall5 = new Walls(340, 250 , 80, 90,Paint.valueOf("BLUE"));
+        wall6 = new Walls(460, 250 , 80, 90,Paint.valueOf("BLUE"));
+        wall7 = new Walls(90, 500 , 130, 110,Paint.valueOf("BLUE"));
+        wall8 = new Walls(290, 500 , 160, 110,Paint.valueOf("BLUE"));
+        wall10 = new Walls(460, 250 , 80, 90,Paint.valueOf("BLUE"));
+        wall11 = new Walls(90, 500 , 130, 110,Paint.valueOf("BLUE"));
+        wall12 = new Walls(290, 500 , 160, 110,Paint.valueOf("BLUE"));
+        wall13 = new Walls(640, 80 , 80, 90,Paint.valueOf("BLUE"));
+        wall14 = new Walls(760, 80 , 80, 90,Paint.valueOf("BLUE"));
+        wall15 = new Walls(640, 250 , 80, 90,Paint.valueOf("BLUE"));
+        wall16 = new Walls(760, 250 , 80, 90,Paint.valueOf("BLUE"));
+        wall17 = new Walls(760, 500 , 130, 110,Paint.valueOf("BLUE"));
+        wall18 = new Walls(500, 500 , 160, 110,Paint.valueOf("BLUE"));
+        entity = new Entity(40,40,20,20, Paint.valueOf("YELLOW"), 5.1f);
 
         Platform.runLater(() -> {
             mainLayout.requestFocus();
@@ -71,53 +109,78 @@ public class HelloController implements Initializable {
     }
 
     private void clearCanvas() {
-        gc.setFill(Paint.valueOf("BLACK"));
-        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        gc.strokeLine(290, 0, 290, 80);
-        gc.strokeLine(290, 440, 290, 320);
 
-        gc.strokeLine(590, 440, 590, 320);
+        gc.setFill(Paint.valueOf("BLACK"));
+
+
+        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+      gc.strokeLine(290, 0, 290, 80  );
+
+      gc.strokeLine(290, 440, 290, 320);
+
+       gc.strokeLine(590, 440, 590, 320);
         gc.strokeLine(200, 400, 400, 400);
         gc.setLineWidth(10);
         gc.setStroke(Paint.valueOf("BLUE")); //Object color
+
         gc.stroke();
-       gc.rect(40, 80 , 80, 90);
-        gc.rect(160, 80 , 80, 90);
-        gc.rect(340, 80 , 80, 90);
-        gc.rect(460, 80 , 80, 90);
+      // gc.rect(40, 80 , 80, 90);
+       // gc.rect(160, 80 , 80, 90);
+      //  gc.rect(340, 80 , 80, 90);
+        //gc.rect(460, 80 , 80, 90);
         gc.setLineWidth(5);
-        gc.rect(40, 250 , 80, 90);
-        gc.rect(160, 250 , 80, 90);
-        gc.rect(340, 250 , 80, 90);
-        gc.rect(460, 250 , 80, 90);
-        gc.rect(90, 500 , 130, 110);
-        gc.rect(290, 500 , 160, 110);
-        gc.rect(640, 80 , 80, 90);
-        gc.rect(760, 80 , 80, 90);
-        gc.rect(640, 250 , 80, 90);
-        gc.rect(760, 250 , 80, 90);
-        gc.rect(760, 500 , 130, 110);
-        gc.rect(500, 500 , 160, 110);
+      //  gc.rect(40, 250 , 80, 90);
+
+//        gc.rect(340, 250 , 80, 90);
+     //   gc.rect(460, 250 , 80, 90);
+      //  gc.rect(90, 500 , 130, 110);
+      //  gc.rect(290, 500 , 160, 110);
+       // gc.rect(640, 80 , 80, 90);
+      //  gc.rect(760, 80 , 80, 90);
+      //  gc.rect(640, 250 , 80, 90);
+      //  gc.rect(760, 250 , 80, 90);
+      //  gc.rect(760, 500 , 130, 110);
+      //  gc.rect(500, 500 , 160, 110);
 
 
     }
 
     public void update() {
         entity.update(inputList);
+
     }
 
     public void render() {
         entity.render(gc);
+        wall1.render(gc);
+        wall2.render(gc);
+        wall3.render(gc);
+        wall4.render(gc);
+        wall5.render(gc);
+        wall6.render(gc);
+        wall7.render(gc);
+        wall8.render(gc);
+        wall9.render(gc);
+        wall10.render(gc);
+        wall11.render(gc);
+        wall12.render(gc);
+        wall13.render(gc);
+        wall14.render(gc);
+        wall15.render(gc);
+        wall16.render(gc);
+        wall17.render(gc);
+        wall18.render(gc);
     }
-
+/*
     private void spawnEntities(int number) {
         Random random = new Random();
         for (int i = 0; i < number; i++) {
             entitiesList.add(new Entity(random.nextInt()));
         }
     }
-
+*/
     public void onMouseMove(MouseEvent mouseEvent) {
+       // if (gc.equals())
         gc.setFill(Paint.valueOf("BLACK"));
         gc.fillOval(mouseEvent.getX(), mouseEvent.getY(), 5, 5);
     }
@@ -130,29 +193,30 @@ public class HelloController implements Initializable {
     public void keyPressed(KeyEvent keyEvent) {
 
 
-
+    // if (gc.equals(Paint.valueOf("BLACK"))){
+if (block = false){
         if (!inputList.contains(keyEvent.getText())) {
             inputList.add(keyEvent.getText());
         }
 
 
         if (keyEvent.getText().equalsIgnoreCase("w")) {
-            entity.setY(entity.getY() - 6);
+            entity.setY(entity.getY() - 1);
 
         }
 
         if (keyEvent.getText().equalsIgnoreCase("s")) {
-            entity.setY(entity.getY() + 6);
+            entity.setY(entity.getY() + 1);
 
 
         }
         if (keyEvent.getText().equalsIgnoreCase("a")) {
-            entity.setX(entity.getX() - 6);
+            entity.setX(entity.getX() - 1);
 
         }
         if (keyEvent.getText().equalsIgnoreCase("d")) {
 
-            entity.setX(entity.getX() + 6);
+            entity.setX(entity.getX() + 1);
         }
 
         gc.setFill(Paint.valueOf("WHITE"));
@@ -160,9 +224,9 @@ public class HelloController implements Initializable {
         gc.setFill(entity.getColor());
         gc.fillRect(entity.getX(), entity.getY(), entity.getWidth(), entity.getHeight());
 
+        }}
 
-    }
-
+  //  }
     public void keyReleased(KeyEvent keyEvent) {
         inputList.remove(keyEvent.getText());
     }
---
+++ PacmanGame/src/main/java/com/example/pacmangame/Walls.java
@@ -0,0 +1,94 @@
+package com.example.pacmangame;
+
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Paint;
+
+import java.util.ArrayList;
+
+public class Walls {
+
+    private double x;
+    private double y;
+    private double width;
+    private double height;
+    private Paint color;
+
+
+    public Walls(int x) {
+    }
+
+
+
+    public Walls(double x, double y, double width, double height, Paint color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+
+    }
+
+    public void update(ArrayList<String> input) {
+        if (input.contains("w")) {
+            setY(getY());
+        }
+
+        if (input.contains("s")) {
+            setY(getY());
+        }
+        if (input.contains("a")) {
+            setX(getX());
+        }
+        if (input.contains("d")) {
+            setX(getX());
+        }
+    }
+
+    public void render(GraphicsContext gc) {
+        gc.setFill(color);
+        gc.fillRect(x, y, width, height);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public Paint getColor() {
+        return color;
+    }
+
+    public void setColor(Paint color) {
+        this.color = color;
+    }
+
+}
